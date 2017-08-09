60de2a8850b4f54a150e5a10f52da6b30fa0b0e1
Rory Cole <rory.cole1990@gmail.com>
2017-08-09 17:15:28 +0100

    Add play by play endpoints

--- new_nba/endpoints/playbyplay.py
+++ new_nba/endpoints/playbyplay.py
@@ -1,25 +1,27 @@
-from nba import enums
-from nba.utils import request_data, parse_result_json
+
+from new_nba import enums
+from new_nba.utils import clean_locals
+from new_nba.endpoints.baseendpoint import BaseEndpoint
 
 
-def playbyplay(GameID, StartPeriod=enums.StartPeriod.Default, EndPeriod=enums.EndPeriod.Default):
-    """
-    Play by play data for a given game, comments from home, away and neutral perspective.
+class PlayByPlay(BaseEndpoint):
 
-    :param GameID: ID of the game to get data for.
-    :type GameID: str
-    :param StartPeriod: starting period filter on the data.
-    :type StartPeriod: nba.nba.bin.enums.StartPeriod
-    :param EndPeriod: ending period filter on the data.
-    :type EndPeriod: nba.nba.bin.enums.EndPeriod
-    :returns: play by play event descriptions and details of players involved
-    :rtype: Dataframe
-
-    """
-    params = locals()
-    endpoint = 'playbyplayv2'
-    r = request_data(params, endpoint)
-    df = parse_result_json(r, 0, 'resultSets')
-    return df
-
-
+    def playbyplay(self, GameID, StartPeriod=enums.StartPeriod.Default, EndPeriod=enums.EndPeriod.Default):
+        """
+        Play by play data for a given game, comments from home, away and neutral perspective.
+    
+        :param GameID: ID of the game to get data for.
+        :type GameID: str
+        :param StartPeriod: starting period filter on the data.
+        :type StartPeriod: nba.nba.bin.enums.StartPeriod
+        :param EndPeriod: ending period filter on the data.
+        :type EndPeriod: nba.nba.bin.enums.EndPeriod
+        :returns: play by play event descriptions and details of players involved
+        :rtype: Dataframe
+    
+        """
+        params = clean_locals(locals())
+        endpoint = 'playbyplayv2'
+        r = self.request(params, endpoint)
+        df = self.process_response(r, 0, 'resultSets')
+        return df
